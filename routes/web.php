cc5e5da2210c35d118488cf4dd76b0832efdc2fd
nicoco1173 <rikurunico@gmail.com>
2022-05-26 17:32:33 +0700

    Add Admin and User Level

--- routes/web.php
+++ routes/web.php
@@ -32,9 +32,13 @@ Route::get('/logout', [LoginController::class, 'logout']) -> name('logout');
 
 
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth','cekLevel:admin'])->group(function () {
     Route::get('/home', [HomePageController::class, 'index']) -> name('HomePage');
     Route::get('/profile', [HomePageController::class, 'profile']) -> name('ProfilePage');
+});
+
+Route::middleware(['auth','cekLevel:user'])->group(function () {
+    Route::get('/home', [HomePageController::class, 'index']) -> name('HomePage');
 });
 
 
