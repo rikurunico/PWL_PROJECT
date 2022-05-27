2cbc233d9f97ba5245e5e04fc79c285420f88698
nicoco1173 <rikurunico@gmail.com>
2022-05-27 21:28:59 +0700

    side bar biar ga ngambang

--- resources/views/AdminView/sidebar.blade.php
+++ resources/views/AdminView/sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="height: 100vh;">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
       <img src="{{ asset('src/img/brandlogo.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
