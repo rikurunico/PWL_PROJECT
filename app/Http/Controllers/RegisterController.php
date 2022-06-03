bcb5331d584238b984608fb1f962e19e36dad269
nicoco1173 <rikurunico@gmail.com>
2022-06-03 17:48:37 +0700

    register default image

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
         $user->notelp = $request->nomor;
         $user->password = bcrypt($request->password);
         $user->level = 'user';
-        $user->foto = 'storage/photo/default.jpg';
+        $user->foto = 'src/img/default.jpg';
         $user->save();
 
         return redirect('/login')->with('success', 'Registration Success! Please Login');
