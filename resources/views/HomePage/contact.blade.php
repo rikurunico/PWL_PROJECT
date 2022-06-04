8aec7611083178c3923b3afbb2c0e7603f30f031
Iqrima <iqrimannisa@gmail.com>
2022-06-04 09:15:15 +0700

    contact page

--- resources/views/HomePage/contact.blade.php
+++ resources/views/HomePage/contact.blade.php
@@ -1,5 +1,39 @@
 @extends('HomePage.layout')
 @section('content')
+<!-- Content Header (Page header) -->
+<section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Contact</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+<!-- Breadcrumb Section Begin -->
+<section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb__text">
+                        <h2>Contact Us</h2>
+                        <div class="breadcrumb__option">
+                            <a href="#">Home</a>
+                            <span>Contact Us</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Breadcrumb Section End -->
+
 
  <!-- Contact Section Begin -->
  <section class="contact spad">
@@ -7,30 +41,30 @@
             <div class="row">
                 <div class="col-lg-3 col-md-3 col-sm-6 text-center">
                     <div class="contact__widget">
-                        <span class="icon_phone"></span>
-                        <h4>Phone</h4>
-                        <p>+01-3-8888-6868</p>
+                        <span class="bi bi-telephone"></span>
+                        <h4>Telepon</h4>
+                        <p>++0341-478888</p>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 text-center">
                     <div class="contact__widget">
-                        <span class="icon_pin_alt"></span>
-                        <h4>Address</h4>
-                        <p>60-49 Road 11378 New York</p>
+                        <span class="bi bi-geo-alt"></span>
+                        <h4>Alamat</h4>
+                        <p>Jl. Soekarno Hatta No 09. Malang 65141</p>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 text-center">
                     <div class="contact__widget">
-                        <span class="icon_clock_alt"></span>
+                        <span class="bi bi-clock"></span>
                         <h4>Open time</h4>
-                        <p>10:00 am to 23:00 pm</p>
+                        <p>8:00 to 22:00 WIB</p>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 text-center">
                     <div class="contact__widget">
-                        <span class="icon_mail_alt"></span>
+                        <span class="bi bi-envelope-open"></span>
                         <h4>Email</h4>
-                        <p>hello@colorlib.com</p>
+                        <p>hellotokofast@colorlib.com</p>
                     </div>
                 </div>
             </div>
@@ -40,16 +74,15 @@
 
     <!-- Map Begin -->
     <div class="map">
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49116.39176087041!2d-86.41867791216099!3d39.69977417971648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886ca48c841038a1%3A0x70cfba96bf847f0!2sPlainfield%2C%20IN%2C%20USA!5e0!3m2!1sen!2sbd!4v1586106673811!5m2!1sen!2sbd"
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d28163.54610241721!2d112.60008712001087!3d-7.940501676869157!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sid!2sid!4v1654308541644!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             height="500" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
         <div class="map-inside">
-            <i class="icon_pin"></i>
+            <i class="bi bi-geo"></i>
             <div class="inside-widget">
-                <h4>New York</h4>
+                <h4>Kota Malang</h4>
                 <ul>
-                    <li>Phone: +12-345-6789</li>
-                    <li>Add: 16 Creek Ave. Farmingdale, NY</li>
+                    <li>Telepon: +0341-478888</li>
+                    <li>Add: Jl. Soekarno Hatta No 09. Malang 65141</li>
                 </ul>
             </div>
         </div>
