3f52a820f83f152a8e5626acb456a6380f50c260
Iqrima <iqrimannisa@gmail.com>
2022-06-05 19:43:38 +0700

    Edit Contact Page

--- resources/views/AdminView/sidebar.blade.php
+++ resources/views/AdminView/sidebar.blade.php
@@ -34,7 +34,7 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item">
+          <li class="nav-item bg-primary">
             <a href="{{ route('HomePageAdmin') }} " class="nav-link {{ ($tittle === "Home Page") ? 'active' : ''}}">
             <i class="bi bi-speedometer2"></i>
               <p>
