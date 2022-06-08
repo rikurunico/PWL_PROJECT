735688e488cd2a2090fa8a66dab16db08ff7dddf
nicoco1173 <rikurunico@gmail.com>
2022-06-08 11:00:30 +0700

    Add Auto Generate Photo

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Storage;
 
 class RegisterController extends Controller
 {
@@ -30,6 +31,9 @@ class RegisterController extends Controller
         $user->notelp = $request->nomor;
         $user->password = bcrypt($request->password);
         $user->level = 'user';
+        $img = \DefaultProfileImage::create($request->name, 256, '#000', '#FFF');
+            Storage::disk('public')->put("profile.png", $img -> encode());
+        $user->foto = 'profile.png';
         $user->save();
 
         return redirect('/login')->with('success', 'Registration Success! Please Login');
