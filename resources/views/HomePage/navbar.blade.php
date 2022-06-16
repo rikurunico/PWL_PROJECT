7cf6700f679a949c23bf0122137830e064b5042b
Iqrima <iqrimannisa@gmail.com>
2022-06-16 13:08:20 +0700

    delete message menu

--- resources/views/HomePage/navbar.blade.php
+++ resources/views/HomePage/navbar.blade.php
@@ -26,39 +26,7 @@
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-      <!-- Navbar Search -->
-      <li class="nav-item">
-        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-          <i class="fas fa-search"></i>
-        </a>
-        <div class="navbar-search-block">
-          <form action="/search" method ="GET">
-            <div class="input-group input-group-sm">
-              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search" value="{{ old('search') }}" >
-              <div class="input-group-append">
-                <button class="btn btn-navbar" type="submit">
-                  <i class="fas fa-search"></i>
-                </button>
-                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                  <i class="fas fa-times"></i>
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </li>
-      <!-- Navbar Dropdown -->
-      <!-- Messages Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-comments"></i>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <!-- Message End -->
-          </a> 
-      </li>
+     
       
       <!-- Notifications Dropdown Menu -->
       <div class="dropdown">
