2986260987710a7b9300915ca58333799b00463e
Iqrima <iqrimannisa@gmail.com>
2022-06-19 21:29:23 +0700

    edit view index

--- resources/views/HomePage/index.blade.php
+++ resources/views/HomePage/index.blade.php
@@ -102,11 +102,7 @@
                 <div class="col-lg-3 col-md-4 col-sm-6 mix dapur mandi instan">
                     <div class="featured__item">
                         <div class="featured__item__pic set-bg" data-setbg="{{'storage/'.$b->gambar}}">
-                            <ul class="featured__item__pic__hover">
-                                <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                            </ul>
+                            
                         </div>
                         <div class="featured__item__text">
                             <h6><a href="#">{{$b ->product}}</a></h6>
