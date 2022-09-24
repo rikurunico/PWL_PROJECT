30193bb177ce1963f84920edb9596beda7b4d277
Iqrima <iqrimannisa@gmail.com>
2022-09-24 14:22:18 +0700

    SupplierTest

--- tests/Feature/SupplierTest.php
+++ tests/Feature/SupplierTest.php
@@ -20,16 +20,6 @@ class SupplierTest extends TestCase
      *
      * @return void
      */
-    
-    // public function setUp(): void
-    // {
-    //     parent::setUp();
-    //     $user = User::factory()->create([
-    //         'level' => 'admin'
-    //     ]);
-    //     $this->supplier = Supplier::factory()->create();
-    // }
-
 
     public function test_open_supplier_page()
     {
@@ -53,20 +43,84 @@ class SupplierTest extends TestCase
 
     public function test_create_supplier()
     {
-        $this->supplier = Supplier::factory()->create();
-        $response = $this->actingAs($user)->post('/dataSupplier', [
-            'nama' => 'Pokoro',
-            'alamat' => 'Malang Kota',
-            'telp' => '09775843'
-        ]);
-        $response->assertStatus(302);
-        $this->assertDatabaseHas('suppliers', [
-            'nama' => 'Pokoro',
-            'alamat' => 'Malang Kota',
-            'telp' => '09775843'
+        $user = User::factory()->create([
+            'level' => 'admin'
         ]);
 
-        $response = $this->followingRedirects()->actingAs($user)->get('/dataSupplier');
-        $response->assertSee('Supplier berhasil ditambahkan');
+        $response = $this->actingAs($user)->post('/postCreateSupplier', [
+            'nama' => 'Supplier 1',
+            'alamat' => 'Jl. Supplier 1',
+            'telp' => '08123456789'
+        ]);
+
+        $response = $this->followingRedirects()
+            ->actingAs($user)
+            ->get('/dataSupplier')
+            ->assertStatus(200);
+    }
+    
+    public function test_read_suplier()
+    {
+        $user = User::factory()->create([
+            'level' => 'admin'
+        ]);
+
+        $supplier = Supplier::factory()->create([
+            'nama' => 'Supplier 1',
+            'alamat' => 'Jl. Supplier 1',
+            'telp' => '08123456789'
+        ]);
+
+        $response = $this->actingAs($user)
+            ->get('/dataSupplier')
+            ->assertSee('Supplier 1');
+    }
+    
+
+    public function test_edit_suplier()
+    {
+        $user = User::factory()->create([
+            'level' => 'admin'
+        ]);
+
+        $supplier = Supplier::factory()->create([
+            'nama' => 'Supplier 1',
+            'alamat' => 'Jl. Supplier 1',
+            'telp' => '08123456789'
+        ]);
+
+        $response = $this->actingAs($user)->post('/postEditSupplier'.$supplier->id, [
+            
+            'nama' => 'Supplier 2',
+            'alamat' => 'Jl. Supplier 2',
+            'telp' => '08123456789'
+        ]);
+
+        $response = $this->followingRedirects()
+            ->actingAs($user)
+            ->get('/dataSupplier')
+            ->assertStatus(200);
+    }
+
+    public function test_delete_supplier()
+    {
+        $user = User::factory()->create([
+            'level' => 'admin'
+        ]);
+
+        $supplier = Supplier::factory()->create([
+            'nama' => 'Supplier 1',
+            'alamat' => 'Jl. Supplier 1',
+            'telp' => '08123456789'
+        ]);
+
+        $response = $this->actingAs($user)->post('/postDeleteSupplier'. $supplier->id, [
+            'id' => $supplier->id,
+        ]);
+
+        $response = $this->followingRedirects()
+            ->actingAs($user)
+            ->get('/dataSupplier')
+            ->assertStatus(200);
     }
 }
